d4e15b21a91be6f3c9f0438e2c1cae91c9f35738
zhujx <ershu.zhujx@menggejk.com>
2022-02-14 18:18:18 +0800

    feat: 启动项目

--- jeecg-boot-base/jeecg-boot-base-core/src/main/java/org/jeecg/config/WebMvcConfiguration.java
+++ jeecg-boot-base/jeecg-boot-base-core/src/main/java/org/jeecg/config/WebMvcConfiguration.java
@@ -54,7 +54,8 @@ public class WebMvcConfiguration implements WebMvcConfigurer {
      */
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/").setViewName("doc.html");
+//        registry.addViewController("/").setViewName("doc.html");
+        registry.addViewController("/").setViewName("index.html");
     }
 
     @Bean
